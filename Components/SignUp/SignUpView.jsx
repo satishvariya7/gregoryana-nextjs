0eeb60699e6d4173e31c7643df48b3edb8f0d297
Satish <satishvariyawork@gmail.com>
2022-07-01 20:52:09 -0700

    commit for added css for login, signup and orders page

--- Components/SignUp/SignUpView.jsx
+++ Components/SignUp/SignUpView.jsx
@@ -1,44 +1,73 @@
 import Image from "next/image";
 import React from "react";
+import { Container } from "react-bootstrap";
 import { deliveryCar, logo } from "../../assets/images";
 import styles from "./SignUpView.module.scss";
 
 const SignUpView = () => {
   return (
-    <div className="row">
-      <div className="col-md-6">
-        <Image src={deliveryCar} alt="car delivery" className="img-fluid" />
+    <Container fluid>
+      <div className='row box-height'>
+        <div className='col-md-6 p-0 img-main'>
+          <Image
+            src={deliveryCar}
+            alt='car delivery'
+            className='img-fluid img-alignment'
+          />
+        </div>
+        <div className='col-md-6 p-0'>
+          <div className='box-set'>
+            <div className='upper-text font-weight-bold'>
+              <p>
+                New to Gregoryana?{" "}
+                <a href='#' className='link-color'>
+                  Book a Demo.
+                </a>
+              </p>
+            </div>
+            <div className='signup-box'>
+              <Image src={logo} alt='site logo' />
+              <div className='mt-4 mb-2'>
+                <h3 className='title mb-2'>Create Account</h3>
+                <p className='text-center'>
+                  Welcome, Please Enter Your Detail!
+                </p>
+              </div>
+              <div className='mailbox mb-3'>
+                <p className='mb-2 font-weight-bold'>Email</p>
+                <input
+                  type='text'
+                  placeholder='Enter Email'
+                  className='w-100 mailbox-height'
+                />
+              </div>
+              <div className='mailbox'>
+                <p className='mb-2'>Password</p>
+                <input
+                  type='text'
+                  placeholder='Enter Password'
+                  className='w-100 mailbox-height'
+                />
+              </div>
+              <div className='my-3 btn-box'>
+                <input type='button' value='Submit' className='mailbtn' />
+              </div>
+              <p className='text-center policy-text'>
+                By using Gregoryana's website, you accept our Terms and Privacy.
+              </p>
+              <div>
+                <span className='login-text'>
+                  Already have an account?{" "}
+                  <a href='#' className='login-link'>
+                    Login
+                  </a>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="col-md-6">
-        <div>
-          <p>
-            New to Gregoryana? <a href="#">Book a Demo.</a>
-          </p>
-        </div>
-        <Image src={logo} alt="site logo" />
-        <div>
-          <h3>Create Account</h3>
-          <p>Welcome, Please Enter Your Detail!</p>
-        </div>
-        <div>
-          <p>Email</p>
-          <input type="text" placeholder="Enter Email" />
-        </div>
-        <div>
-          <p>Password</p>
-          <input type="text" placeholder="Enter Password" />
-        </div>
-        <div>
-          <input type="button" value="Submit" />
-        </div>
-        <p>By using Gregoryana's website, you accept our Terms and Privacy.</p>
-        <div>
-          <span>
-            Already have an account? <a href="#">Login</a>
-          </span>
-        </div>
-      </div>
-    </div>
+    </Container>
   );
 };
 
--- Components/SimpleLayout.jsx
+++ Components/SimpleLayout.jsx
@@ -5,12 +5,13 @@ export default function SimpleLayout(props) {
   return (
     <>
       {/* <Navbar /> */}
-      <div className="row">
-        <div className="col-md-3">
+
+      <div className='row m-0 sidebar-bg'>
+        <div className='col-md-2 p-0'>
           <SideBar />
         </div>
-        <div className="col-md-9">
-          <main role="main">{props.children}</main>
+        <div className='col-md-10'>
+          <main role='main'>{props.children}</main>
         </div>
       </div>
     </>
